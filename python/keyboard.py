571598f40e377a50e178c8c91cb6eb4c6b66e5f2
mascygp <andrea@AMASCIADRI-LAP.localdomain>
2021-02-18 22:16:04 +0100

    addes symbols management

--- python/keyboard.py
+++ python/keyboard.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 import wx
+import time
 
 
 class KeyBoard(wx.Frame):
     def __init__(self, text='') : #, style = wx.SYSTEM_MENU):
-        wx.Frame.__init__(self, None, pos=(25,110), size=(460, 240), title="keypad")
+        wx.Frame.__init__(self, None, pos=(25,110), size=(430, 270), title="keypad")
 
-        self.char_case = '1'
+        self.char_case = 'LO'
         self.shift = False
+        self.symbols = False
+        self.bn_downtime = 0
+        self.bn_duration = 0
 
         # graphic
         self.text_initial = text
@@ -35,83 +39,109 @@ class KeyBoard(wx.Frame):
 
         # Matrix of keyboard
         self.keymap1 = [
-                [ 'bn_1', 'bn_2', 'bn_3', 'bn_4', 'bn_5', 'bn_6', 'bn_7', 'bn_8', 'bn_9',   'bn_0'   , 'OK'    ],
-                [ 'bn_q', 'bn_w', 'bn_e', 'bn_r', 'bn_t', 'bn_y', 'bn_u', 'bn_i', 'bn_o',   'bn_p'   , 'BS'    ],
-                [ 'CL'  , 'bn_a', 'bn_s', 'bn_d', 'bn_f', 'bn_g', 'bn_h', 'bn_j', 'bn_k',   'bn_l'   , 'C'     ],
-                [ 'SH'  , 'bn_z', 'bn_x', 'bn_c', 'bn_v', 'bn_b', 'bn_n', 'bn_m', 'bn_dot', 'bn_dash', 'Close' ]
+                [ 'bn_1', 'bn_2', 'bn_3', 'bn_4', 'bn_5', 'bn_6', 'bn_7', 'bn_8', 'bn_9',   'bn_0'   ],
+                [ 'bn_q', 'bn_w', 'bn_e', 'bn_r', 'bn_t', 'bn_y', 'bn_u', 'bn_i', 'bn_o',   'bn_p'   ],
+                [ '_SP' , 'bn_a', 'bn_s', 'bn_d', 'bn_f', 'bn_g', 'bn_h', 'bn_j', 'bn_k',   'bn_l'   ],
+                [ 'CL'  , 'bn_z', 'bn_x', 'bn_c', 'bn_v', 'bn_b', 'bn_n', 'bn_m', 'BS' ],
+                [ 'SH'  , 'SYM', 'Space', 'Close', 'OK']
         ]
 
-        # buttons - 1: normal - 2: shift
+        """
+        LO : normal char
+        SH : shift button pressed 
+        SYM: symbols button pressed 
+        """
         self.bn = {
-                'bn_a': { '1': 'a', '2': 'A' },
-                'bn_b': { '1': 'b', '2': 'B' },
-                'bn_c': { '1': 'c', '2': 'C' },
-                'bn_d': { '1': 'd', '2': 'D' },
-                'bn_e': { '1': 'e', '2': 'E' },
-                'bn_f': { '1': 'f', '2': 'F' },
-                'bn_g': { '1': 'g', '2': 'G' },
-                'bn_h': { '1': 'h', '2': 'H' },
-                'bn_i': { '1': 'i', '2': 'I' },
-                'bn_j': { '1': 'j', '2': 'J' },
-                'bn_k': { '1': 'k', '2': 'K' },
-                'bn_l': { '1': 'l', '2': 'L' },
-                'bn_m': { '1': 'm', '2': 'M' },
-                'bn_n': { '1': 'n', '2': 'N' },
-                'bn_o': { '1': 'o', '2': 'O' },
-                'bn_p': { '1': 'p', '2': 'P' },
-                'bn_q': { '1': 'q', '2': 'Q' },
-                'bn_r': { '1': 'r', '2': 'R' },
-                'bn_s': { '1': 's', '2': 'S' },
-                'bn_t': { '1': 't', '2': 'T' },
-                'bn_u': { '1': 'u', '2': 'U' },
-                'bn_v': { '1': 'v', '2': 'V' },
-                'bn_w': { '1': 'w', '2': 'W' },
-                'bn_x': { '1': 'x', '2': 'X' },
-                'bn_y': { '1': 'y', '2': 'Y' },
-                'bn_z': { '1': 'z', '2': 'Z' },
+                'bn_a': { 'LO': 'a', 'SH': 'A', 'SYM': '@' },
+                'bn_b': { 'LO': 'b', 'SH': 'B', 'SYM': '.' },
+                'bn_c': { 'LO': 'c', 'SH': 'C', 'SYM': '.' },
+                'bn_d': { 'LO': 'd', 'SH': 'D', 'SYM': '.' },
+                'bn_e': { 'LO': 'e', 'SH': 'E', 'SYM': '[' },
+                'bn_f': { 'LO': 'f', 'SH': 'F', 'SYM': '.' },
+                'bn_g': { 'LO': 'g', 'SH': 'G', 'SYM': '.' },
+                'bn_h': { 'LO': 'h', 'SH': 'H', 'SYM': '.' },
+                'bn_i': { 'LO': 'i', 'SH': 'I', 'SYM': '>' },
+                'bn_j': { 'LO': 'j', 'SH': 'J', 'SYM': '.' },
+                'bn_k': { 'LO': 'k', 'SH': 'K', 'SYM': '.' },
+                'bn_l': { 'LO': 'l', 'SH': 'L', 'SYM': '.' },
+                'bn_m': { 'LO': 'm', 'SH': 'M', 'SYM': '.' },
+                'bn_n': { 'LO': 'n', 'SH': 'N', 'SYM': '.' },
+                'bn_o': { 'LO': 'o', 'SH': 'O', 'SYM': '_' },
+                'bn_p': { 'LO': 'p', 'SH': 'P', 'SYM': '-' },
+                'bn_q': { 'LO': 'q', 'SH': 'Q', 'SYM': '\\'},
+                'bn_r': { 'LO': 'r', 'SH': 'R', 'SYM': ']' },
+                'bn_s': { 'LO': 's', 'SH': 'S', 'SYM': '#' },
+                'bn_t': { 'LO': 't', 'SH': 'T', 'SYM': '{' },
+                'bn_u': { 'LO': 'u', 'SH': 'U', 'SYM': '<' },
+                'bn_v': { 'LO': 'v', 'SH': 'V', 'SYM': '.' },
+                'bn_w': { 'LO': 'w', 'SH': 'W', 'SYM': '|' },
+                'bn_x': { 'LO': 'x', 'SH': 'X', 'SYM': '.' },
+                'bn_y': { 'LO': 'y', 'SH': 'Y', 'SYM': '}' },
+                'bn_z': { 'LO': 'z', 'SH': 'Z', 'SYM': '.' },
 
-                'bn_0': { '1': '0', '2': '0' },
-                'bn_1': { '1': '1', '2': '1' },
-                'bn_2': { '1': '2', '2': '2' },
-                'bn_3': { '1': '3', '2': '3' },
-                'bn_4': { '1': '4', '2': '4' },
-                'bn_5': { '1': '5', '2': '5' },
-                'bn_6': { '1': '6', '2': '6' },
-                'bn_7': { '1': '7', '2': '7' },
-                'bn_8': { '1': '8', '2': '8' },
-                'bn_9': { '1': '9', '2': '9' },
+                'bn_0': { 'LO': '0', 'SH': '0', 'SYM': '=' },
+                'bn_1': { 'LO': '1', 'SH': '1', 'SYM': '!' },
+                'bn_2': { 'LO': '2', 'SH': '2', 'SYM': '"' },
+                'bn_3': { 'LO': '3', 'SH': '3', 'SYM': ''  },
+                'bn_4': { 'LO': '4', 'SH': '4', 'SYM': '$' },
+                'bn_5': { 'LO': '5', 'SH': '5', 'SYM': '%' },
+                'bn_6': { 'LO': '6', 'SH': '6', 'SYM': '&' },
+                'bn_7': { 'LO': '7', 'SH': '7', 'SYM': '/' },
+                'bn_8': { 'LO': '8', 'SH': '8', 'SYM': '(' },
+                'bn_9': { 'LO': '9', 'SH': '9', 'SYM': ')' },
         
-                'bn_dot':  { '1': '.', '2': ',' },
-                'bn_dash': { '1': '-', '2': '_' },
-
-                'SH': { '1': 'SH', '2': 'SH' },    # Shift
-                'CL': { '1': 'CL', '2': 'CL' },    # Caps Locks
-                'BS': { '1': '<',  '2': '<'  },    # Back Space
-                'C':  { '1': 'C',  '2': 'C'  },    # Clear
-                'OK': { '1': 'OK', '2': 'OK' },
-                'Close': { '1': 'Close', '2': 'Close' },
+                'SYM':   { 'LO': '=\<',     'SH': '=\<',    'SYM': 'abc'    },    # SYMBOLS
+                'CL':    { 'LO': 'CapsLk',  'SH': 'CapsLk', 'SYM': 'CapsLk' },    # Caps Locks
+                'SH':    { 'LO': 'Shift',   'SH': 'Shift',  'SYM': 'Shift'  },    # Shift
+                'BS':    { 'LO': '<--',     'SH': '<--',    'SYM': '<--'    },    # Back Space
+                'Space': { 'LO': '',        'SH': '',       'SYM': ''       },    # bar space
+                'OK':    { 'LO': 'Enter',   'SH': 'Enter',  'SYM': 'Enter'  },
+                'Close': { 'LO': 'Close',   'SH': 'Close',  'SYM': 'Close'  }
+                #'C':     { 'LO': 'C',       'SH': 'C',      'SH': 'C'       },    # Clear
                 }
 
     def keyboard_ui(self, keymap, ccase):
         vbox = wx.BoxSizer(wx.VERTICAL)
         for row in keymap:
             hbox = wx.BoxSizer(wx.HORIZONTAL)
+
             for k in row:
-                self.bn[k]['obj'] = wx.Button(self, label=self.bn[k][ccase], size=self.bn_size)
-                hbox.Add( self.bn[k]['obj'] )
+
+                # spacer
+                if k == '_SP':
+                    hbox.AddSpacer(self.bn_size[0] * 0.5)
+                else:
+                    # set bn size
+                    if k == 'Space':
+                        bn_size = (self.bn_size[0] * 4, self.bn_size[1])
+                    elif k in ('CL', 'BS', 'SH', 'SYM', 'OK', 'Close'):
+                        bn_size = (self.bn_size[0] * 1.5, self.bn_size[1])
+                    else:
+                        bn_size = self.bn_size
+
+                    # create bn
+                    self.bn[k]['obj'] = wx.Button(self, label=self.bn[k][ccase], size=bn_size)
+                    hbox.Add( self.bn[k]['obj'] )
+
             vbox.Add(hbox)
 
-        # Bind
+        # Bind buttons
         for k in self.bn.keys():
             if k[0:3] == 'bn_':
+                print(k)
                 self.bn[k]['obj'].Bind(wx.EVT_BUTTON, self.OnButton)
+
+        # if keep pressed BackSlash, clear input
+        self.bn['BS']['obj'].Bind(wx.EVT_LEFT_DOWN, self.OnBackSpaceDown)
+        self.bn['BS']['obj'].Bind(wx.EVT_LEFT_UP, self.OnBackSpaceUp)
+        self.bn['BS']['obj'].Bind(wx.EVT_BUTTON, self.OnBackSpace)
 
         self.bn['OK']['obj'].Bind(wx.EVT_BUTTON, self.OnOk)
         self.bn['Close']['obj'].Bind(wx.EVT_BUTTON, self.OnClose)
-        self.bn['BS']['obj'].Bind(wx.EVT_BUTTON, self.OnBackSpace)
-        self.bn['C']['obj'].Bind(wx.EVT_BUTTON, self.OnClear)
         self.bn['SH']['obj'].Bind(wx.EVT_BUTTON, self.OnShift)
         self.bn['CL']['obj'].Bind(wx.EVT_BUTTON, self.OnCapsLk)
+        self.bn['SYM']['obj'].Bind(wx.EVT_BUTTON, self.OnSymbols)
+        #self.bn['C']['obj'].Bind(wx.EVT_BUTTON, self.OnClear)
 
         return vbox
 
@@ -119,24 +149,40 @@ class KeyBoard(wx.Frame):
         self.input.AppendText( e.GetEventObject().GetLabel() )
         if self.shift:
             self.shift = False
-            self.change_layout('1')
+            self.change_layout('LO')
 
     def OnShift(self, e):
+        print(e.GetTimestamp())
         self.shift = True
-        self.change_layout('2')
+        self.change_layout('SH')
 
     def OnCapsLk(self, e):
-        if self.char_case == '1':
-            self.bn['CL']['obj'].SetBackgroundColour(self.color_pushed)
-            self.char_case = '2'
-            self.change_layout('2')
+        if self.char_case == 'LO':
+            self.change_layout('CL')
         else:
-            self.bn['CL']['obj'].SetBackgroundColour(self.color_default)
-            self.char_case = '1'
-            self.change_layout('1')
+            self.change_layout('LO')
+
+    def OnSymbols(self, e):
+        if self.symbols:
+            self.change_layout('LO')
+        else:
+            self.change_layout('SYM')
+
+    def OnBackSpaceDown(self, e):
+        self.bn_downtime = time.time()
+        e.Skip()
+
+    def OnBackSpaceUp(self, e):
+        bn_uptime = time.time()
+        self.bn_duration = bn_uptime - self.bn_downtime 
+        self.bn_downtime = 0
+        e.Skip()
 
     def OnBackSpace(self, e):
-        self.input.SetValue(self.input.GetValue()[:-1])
+        if self.bn_duration < 0.6:
+            self.input.SetValue(self.input.GetValue()[:-1])
+        else:
+            self.input.SetValue('')
 
     def OnClear(self, e):
         self.input.SetValue('')
@@ -148,6 +194,41 @@ class KeyBoard(wx.Frame):
 
     def OnClose(self, e):
         self.Close()
+
+    def change_layout(self, ccase):
+        if ccase == 'LO':
+            self.char_case = 'LO'
+            self.bn['CL']['obj'].SetBackgroundColour(self.color_default)
+            self.symbols = False
+            self.bn['SYM']['obj'].SetBackgroundColour(self.color_default)
+
+        elif ccase == 'SH':
+            self.symbols = False
+            self.bn['SYM']['obj'].SetBackgroundColour(self.color_default)
+
+        elif ccase == 'CL':
+            ccase = 'SH'
+            self.char_case = 'SH'
+            self.bn['CL']['obj'].SetBackgroundColour(self.color_pushed)
+            self.symbols = False
+            self.bn['SYM']['obj'].SetBackgroundColour(self.color_default)
+
+        elif ccase == 'SYM':
+            self.char_case = 'LO'
+            self.bn['CL']['obj'].SetBackgroundColour(self.color_default)
+            self.symbols = True
+            self.bn['SYM']['obj'].SetBackgroundColour(self.color_pushed)
+
+        for i in self.bn:
+            self.bn[i]['obj'].SetLabel(self.bn[i][ccase])
+
+
+if __name__ == "__main__":
+    app = wx.App()
+    m = KeyBoard()
+    m.Show()
+    app.MainLoop()
+
 
     def change_layout(self, ccase):
         for i in self.bn:
